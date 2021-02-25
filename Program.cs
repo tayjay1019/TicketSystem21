3a3d3afa10a924cb1bc250378bc8dfeff05a1c87
tayjay1019 <tjoseph1@my.wctc.edu>
2021-02-25 10:41:58 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketSystem21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
