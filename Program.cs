7b3fc1108871f6a3ea8e2e69c2243f2712b08792
tayjay1019 <tjoseph1@my.wctc.edu>
2021-02-25 11:00:17 -0600

    added Ticket class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
+using System.Collections.Generic;
 
 namespace TicketSystem21
 {
     class Program
     {
+        // create static instance of Logger
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Info("Program started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
         }
     }
 }
---
+++ Ticket.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace TicketSystem21
+{
+    public class Ticket
+    {
+        public UInt64 ticketId { get; set; }
+        public string summary { get; set; }
+        public string status { get; set; }
+        public string priority { get; set; }
+        public string submitter { get; set; }
+        public string assigned { get; set;}
+        public List<string> watching { get; set; }
+
+        public Ticket()
+        {
+            watching = new List<string>();
+        }
+
+        public string Display()
+        {
+            return $"id: {ticketId}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmiter: {submitter}\nWatching: {string.Join(", ", watching)}\n";
+        }
+    }
+}
